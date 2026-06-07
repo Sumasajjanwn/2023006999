5a21c343fbe98b78525f952cb0718341bfa4fb8f
sumalatha <sumasajjanwn070@gmail.com>
2026-06-07 11:10:28 +0530

    Added middleware and notification backend

---
+++ notification_app_be/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Campus Notification Running"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server started");
+});
